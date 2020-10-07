e572569a7493f79c0ae394f559750dfbcddb085a
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-07 09:39:24 +0530

    (Samiul Mamud) Added:Taking User Input Logic is added

--- src/com/capgemini/workshop/TicTacToeGame.java
+++ src/com/capgemini/workshop/TicTacToeGame.java
@@ -1,8 +1,23 @@
 package com.capgemini.workshop;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	public char[] createTicTacBoard(int size) {
 		char[] boardSize = new char[size];
 		return boardSize;
+	}
+
+	public void takeInputFromPlayer(Boolean player, Boolean computer, Scanner scan) {
+		System.out.println("Enter 'X' or 'O' ");
+		String entry = scan.next();
+		if (entry.equals("X")) {
+			player = true;
+			computer = false;
+		} else if (entry.equals("O")) {
+			player = false;
+			computer = true;
+		} else
+			System.out.println("Invalid Input!");
 	}
 }
