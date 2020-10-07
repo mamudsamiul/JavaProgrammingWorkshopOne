972b5e85e93b8da6cfe44809953c967fe6f3be58
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-07 12:58:51 +0530

    (Samiul Mamud) Added: Tossing logic is added for 1st turn

--- src/com/capgemini/workshop/Execotor.java
+++ src/com/capgemini/workshop/Execotor.java
@@ -8,21 +8,21 @@ public class Execotor {
 
 	public static void main(String[] args) {
 		int location;
+		boolean toss;
 		System.out.println("-----Welcome to Tik Tak Toe Game-----");
 		Scanner scan = new Scanner(System.in);
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
 		char[] board = ticTacToeGame.createTicTacBoard(10);
-		ticTacToeGame.takeInputFromPlayer(scan);
-		System.out.println("player input is : " + player);
-		ticTacToeGame.showBoard(board);
-		location = ticTacToeGame.chooseLocation(board, scan);
-		board[location] = player;
-		ticTacToeGame.showBoard(board);
-		location = ticTacToeGame.chooseLocation(board, scan);
-		board[location] = player;
-		ticTacToeGame.showBoard(board);
-		location = ticTacToeGame.chooseLocation(board, scan);
-		board[location] = player;
-		ticTacToeGame.showBoard(board);
+		toss=ticTacToeGame.toss();
+		if(toss==true) {
+			System.out.println("It is Your Turn");
+			ticTacToeGame.takeInputFromPlayer(scan);
+			System.out.println("player input is : " + player);
+			ticTacToeGame.showBoard(board);
+			location = ticTacToeGame.chooseLocation(board, scan);
+			board[location] = player;
+		}
+		else
+			System.out.println("Computer turn");
 	}
 }
