1273f1c2d7790537262b420249e18a331ba7379e
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-07 12:15:40 +0530

    (Samiul Mamud) Added: Multiple entry in sigle position validation logic is added

--- src/com/capgemini/workshop/Execotor.java
+++ src/com/capgemini/workshop/Execotor.java
@@ -7,6 +7,7 @@ public class Execotor {
 	static char computer;
 
 	public static void main(String[] args) {
+		int location;
 		System.out.println("-----Welcome to Tik Tak Toe Game-----");
 		Scanner scan = new Scanner(System.in);
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
@@ -14,7 +15,13 @@ public class Execotor {
 		ticTacToeGame.takeInputFromPlayer(scan);
 		System.out.println("player input is : " + player);
 		ticTacToeGame.showBoard(board);
-		int location = ticTacToeGame.chooseLocation(board, scan);
+		location = ticTacToeGame.chooseLocation(board, scan);
+		board[location] = player;
+		ticTacToeGame.showBoard(board);
+		location = ticTacToeGame.chooseLocation(board, scan);
+		board[location] = player;
+		ticTacToeGame.showBoard(board);
+		location = ticTacToeGame.chooseLocation(board, scan);
 		board[location] = player;
 		ticTacToeGame.showBoard(board);
 	}
